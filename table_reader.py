615764a83aa0f9da15c705196ff66df023e6b06f
Riley Haffner <haffnerriley@wustl.edu>
2023-10-03 13:58:16 -0500

    added some new features

--- table_reader.py
+++ table_reader.py
@@ -1,22 +1,23 @@
 
+#!/usr/bin/env python3
 from __future__ import print_function
 import PySimpleGUI as sg
 
-#!/usr/bin/env python3
+
 
 import time
 from datetime import datetime
-import mercury
+#import mercury
 
 
 
 # Define the GUI layout
 layout = [
-    [sg.Text("Reader Path:"), sg.InputText( key="connect-reader")],
-    [sg.Text("Set Read Power (0-2700):"), sg.InputText(key="reader-power")]
+    [sg.Text("Device URI:"), sg.InputText( key="connect-reader")],
+    [sg.Text("Set Read Power (0-2700):"), sg.InputText(key="reader-power")],
     [sg.Text("EPC to Update:"), sg.InputText(key="epc")],
-    [sg.Text("New Item Name":), sg.InputText(key="item-name")],
-    [sg.Button("Connect"), sg.Button("Set Power"), sg.Button("Find EPC"), sg.Button("Update Item")],
+    [sg.Text("New Item Name:"), sg.InputText(key="item-name")],
+    [sg.Button("Connect"), sg.Button("Set Power"), sg.Button("Find Item to Update"), sg.Button("Update Item")],
     [sg.Multiline("", size=(40, 10), key="-EventLog-", disabled=True)]
 ]
 
@@ -24,7 +25,7 @@ layout = [
 window = sg.Window("Smart Kitchen Application", layout, finalize=True)
 
 reader = "undefined"
-
+reader_status = "disconnected"
 # Event loop
 while True:
     event, values = window.read()
@@ -36,23 +37,58 @@ while True:
     elif event == "Connect":
         #Connect to reader using usb 
         reader_port = values["connect-reader"]
-        reader = mercury.Reader(reader_port, baudrate=115200)
-
+        try:
+            reader = mercury.Reader(reader_port, baudrate=115200)
+        except:
+            reader_status = "disconnected"
+            window["-EventLog-"].print(f"Falied to connect to Reader! Please check device URI!\n")
+            continue
+        
+        
         #Printing reader model and region to console
         window["-EventLog-"].print(f"Reader Connected: Model {reader.get_model()}\n")
         window["-EventLog-"].print(f"Supported Regions: {reader.get_supported_regions()}\n")
         
-        #Set the reader region
+        #Set the reader region and default power
         reader.set_region("NA2")
         reader.set_read_plan([1], "GEN2", read_power=1900)
+        reader_status = "connected"
     elif event == "Set Power":
+        #Grabbing power value from input box
+        #Could add some logic to make sure input values are correct but will save for later if have time...
+        if(reader_status == "disconnected"):
+            window["-EventLog-"].print(f"Please connect to reader first!\n")
+            continue
         reader_power = values["reader-power"]
-        reader.set_read_plan([1], "GEN2", read_power=reader_power)
-        window["-EventLog-"].print(f"Button 2 clicked. Input 2 value: {input2_value}\n")
-    elif event == "button 1":
-        input1_value = values["epc-write-id"] 
-        reader.read()
-        window["-EventLog-"].print(f"Button 1 clicked. Input 1 value: {input1_value}\n")
+        try:
+            #Set the reader power, protocol, and number of antennas
+            reader.set_read_plan([1], "GEN2", read_power=reader_power)
+        except:
+            window["-EventLog-"].print(f"Failed to set reader power!\n")
+            continue
+        
+        window["-EventLog-"].print(f"Reader power set to {reader_power}\n")
+    elif event == "Find Item to Update":
+        if(reader_status == "disconnected"):
+            window["-EventLog-"].print(f"Please connect to reader first!\n")
+            continue
+
+        try:
+             reader_start_val = reader.read()
+             window["-EventLog-"].print(f"Ready to Read! {reader_start_val}\n")
+        except:
+            window["-EventLog-"].print(f"Failed to start reading!\n")
+            continue
+        
+        try:
+            epcs = map(lambda tag: tag.epc, reader.read())
+            window["-EventLog-"].print(f"Failed to start reading!\n")
+            print(list(epcs))
+        except:
+            window["-EventLog-"].print(f"Failed to start reading!\n")
+            continue
+    elif event == "Update Item":
+
 
 # Close the window
 window.close()
