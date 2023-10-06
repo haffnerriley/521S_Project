ddc6e2f26a527e484e2c6f3212ff15663cb1e5b8
ebiernacki <ebiernacki@wustl.edu>
2023-10-06 13:43:43 -0500

    updates to use name in continous reading

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=115200)
 
 print(reader.get_model())
 # print(reader.get_supported_regions())
-
+## print get reader power!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 reader.set_region("NA2")
 reader.set_read_plan([1], "GEN2", read_power=1900) #upper lower bounds?????
 print(reader.get_temperature())
