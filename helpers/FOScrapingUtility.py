13dca8f4fe382a18bce49fb219da262ff59c5ae7
Tyler <martin.t@wustl.edu>
2023-10-12 14:02:33 -0500

    forgot to add zoo moving

--- helpers/FOScrapingUtility.py
+++ helpers/FOScrapingUtility.py
@@ -7,7 +7,7 @@ classes = " "
 
 #I'm definitely going to forget how to use this at some point
 if(sys.argv[1] == "-h" or sys.argv[1] == "--help"):
-    print("Usage: FOScrapingUtility.py <coco export path (relative)> <class names (space separated)> ")
+    print("Usage: FOScrapingUtility.py <coco export path (relative)> <dataset name> <class names (space separated)>")
     sys.exit(0)
 
 #check if export dir exists and make if doesnt
@@ -15,7 +15,7 @@ if (not os.path.isdir(sys.argv[1])):
     os.mkdir(sys.argv[1])
 
 # The Dataset or DatasetView containing the samples you wish to export
-dataset_or_view = foz.load_zoo_dataset("coco-2017", classes = [classes.join(sys.argv[2:])], label_types=["detections", "segmentations"], splits="train")
+dataset_or_view = foz.load_zoo_dataset(sys.argv[2], classes = [classes.join(sys.argv[3:])], label_types=["detections", "segmentations"], splits="train")
 
 # The directory to which to write the exported dataset
 export_dir = sys.argv[1]
