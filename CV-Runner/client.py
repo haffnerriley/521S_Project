ccd3c16d4ccd3f9ab241f3c9d9d1105e2efb88c7
Tyler <martin.t@wustl.edu>
2023-12-06 15:09:53 -0600

    added cam server loop

--- CV-Runner/client.py
+++ CV-Runner/client.py
@@ -6,10 +6,6 @@ import time
 import pickle
 import zlib
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('172.27.47.22', 54321))
-connection = client_socket.makefile('wb')
-
 cam = cv2.VideoCapture(0)
 
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -22,20 +18,31 @@ encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 while True:
 
-    #store frame in shared memory
-    if frame_counter == 10:
-        frame_counter = 0
+    try:
 
-        ret, frame = cam.read()
-        result, frame = cv2.imencode('.jpg', frame, encode_param)
-        data = pickle.dumps(frame, 0)
-        size = len(data)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('172.27.47.22', 54321))
+        connection = client_socket.makefile('wb')
+
+        while True:
+
+            #store frame in shared memory
+            if frame_counter == 10:
+                frame_counter = 0
+
+                ret, frame = cam.read()
+                result, frame = cv2.imencode('.jpg', frame, encode_param)
+                data = pickle.dumps(frame, 0)
+                size = len(data)
 
 
-        print("{}: {}".format(img_counter, size))
-        client_socket.sendall(struct.pack(">L", size) + data)
-        img_counter += 1
-    
-    frame_counter += 1
+                print("{}: {}".format(img_counter, size))
+                client_socket.sendall(struct.pack(">L", size) + data)
+                img_counter += 1
+            
+            frame_counter += 1
+
+    except Exception as e:
+        print("main receiver turned off....\nlooping until needed.....")
 
 cam.release()
--- CV-Runner/cv.py
+++ CV-Runner/cv.py
@@ -215,16 +215,15 @@ else:
             #read from server camera
             while True:
                 while len(data) < payload_size:
-                    print("Recv: {}".format(len(data)))
                     data += conn.recv(4096)
 
-                print("Done Recv: {}".format(len(data)))
                 packed_msg_size = data[:payload_size]
                 data = data[payload_size:]
+                
                 msg_size = struct.unpack(">L", packed_msg_size)[0]
-                print("msg_size: {}".format(msg_size))
                 while len(data) < msg_size:
                     data += conn.recv(4096)
+                    
                 frame_data = data[:msg_size]
                 data = data[msg_size:]
 
